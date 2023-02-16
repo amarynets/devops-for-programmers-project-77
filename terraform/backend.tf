398ecd0fa38b0014ba9169879c7d90196b664b1c
amarynets <andrii.marynets@gmail.com>
2023-02-16 11:57:32 +0200

    Change provider

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,15 +5,6 @@ terraform {
             name = "amarynets"
         }
     }
-    required_providers {
-        random = {
-            source = "hashicorp/random"
-            version = "3.0.1"
-        }
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-            version = "2.26.0"
-        }
-    }
+    
     required_version = ">= 1.1.0"
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+        random = {
+            source = "hashicorp/random"
+            version = "3.0.1"
+        }
+        digitalocean = {
+            source = "digitalocean/digitalocean"
+            version = "2.26.0"
+        }
+    }
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
